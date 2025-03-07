ff485a2e930b319d633f02ea9df776c3bd5d3aef
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-03-07 19:46:34 +0300

    arrow functions lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide =function(a, b){
+    return a / b;
+};
+// testing
+console.log(divide(100, 5));
+
+const square = (x) => {
+  return x * x;
+};
+
+const add = (a, b) => {
+  return a + b;
+};
